de6f3974e5b0b03ecfd1d175af89a9617558eb09
majorwaves <majorwaves@gmail.com>
2023-10-13 14:24:55 -0600

    update to 2k resolution screen shots

--- functions/screenshot/screenshot.js
+++ functions/screenshot/screenshot.js
@@ -1,60 +1,62 @@
-const chromium = require("chrome-aws-lambda")
-const defaults = require("lodash.defaults")
-const qs = require("qs")
-const regexMerge = require("regex-merge")
+const chromium = require("chrome-aws-lambda");
+const defaults = require("lodash.defaults");
+const qs = require("qs");
+const regexMerge = require("regex-merge");
 
 const pattern = regexMerge(
   /^(?:\/\.netlify\/functions)?/,
   /(?:\/screenshot)?/,
   /(?:\/(?<width>[0-9]+)x(?<height>[0-9]+))?/,
   /(?<path>\/.*?)/,
-  /(?:\.png)?$/,
-)
+  /(?:\.png)?$/
+);
 
 const options = {
   base: process.env.BASE_URL,
-  width: 1920,
-  height: 1080,
+  width: 2560,
+  height: 1440,
   maxage: 60 * 60 * 24 * 7,
-}
+};
 
 exports.handler = async (event, context) => {
   const { base, path, width, height, maxage } = (() => {
-    const settings = defaults(event.path.match(pattern).groups, options)
+    const settings = defaults(event.path.match(pattern).groups, options);
 
-    settings.width = parseInt(settings.width)
-    settings.height = parseInt(settings.height)
+    settings.width = parseInt(settings.width);
+    settings.height = parseInt(settings.height);
 
-    return settings
-  })()
+    return settings;
+  })();
 
-  const url = `${base}${path}${qs.stringify(event.queryStringParameters, { addQueryPrefix: true })}`
+  const url = `${base}${path}${qs.stringify(event.queryStringParameters, {
+    addQueryPrefix: true,
+  })}`;
 
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
     headless: chromium.headless,
-  })
+  });
 
-  const page = await browser.newPage()
+  const page = await browser.newPage();
 
-  await page.setViewport({ width, height })
+  await page.setViewport({ width, height });
 
-  await page.goto(url, { waitUntil: "networkidle2" })
+  await page.goto(url, { waitUntil: "networkidle2" });
 
-  const screenshot = await page.screenshot()
+  const screenshot = await page.screenshot();
 
-  await browser.close()
+  await browser.close();
 
   return {
     statusCode: 200,
     headers: {
       "Cache-Control": `public, max-age=${maxage}`,
       "Content-Type": "image/png",
-      "Expires": new Date(Date.now() + maxage * 1000).toUTCString(),
+      Expires: new Date(Date.now() + maxage * 1000).toUTCString(),
     },
     body: screenshot.toString("base64"),
     isBase64Encoded: true,
-  }
-}
+  };
+};
